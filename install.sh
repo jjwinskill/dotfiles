9e553e0fcde334d9abba3a904d04290fc51939d5
Josh Winskill <jjwinskill@Joshs-MacBook-Pro.local>
2024-06-18 14:37:40 -0700

    hmm

--- install.sh
+++ install.sh
@@ -23,10 +23,6 @@ zshrc() {
     cat .p10k.zsh > $HOME/.p10k.zsh
 }
 
-# change time zone
-sudo ln -fs /usr/share/zoneinfo/America/Chicago /etc/localtime
-sudo dpkg-reconfigure --frontend noninteractive tzdata
-
 zshrc
 
 # make directly highlighting readable - needs to be after zshrc line
